683f4417c91c110814223ca48bf5eedda79d34ed
julien-lab <julienn.ndiaye59155@gmail.com>
2022-02-17 17:40:23 +0100

    add effect change for one sound

--- public/js/connectedTableCoreFunction.js
+++ public/js/connectedTableCoreFunction.js
@@ -5,16 +5,44 @@ function testff(btn){
     bouton.innerHTML = "&#9658;"
 }
 
-function addFilter(audioName,btn , circle, boutoncolor){
-    var context = new AudioContext(),
-        audioSource = context.createMediaElementSource(document.getElementById(audioName)),
+const MEDIA_ELEMENT_NODES = new WeakMap();
+let context = []
+let allAudioEffect;
+
+function addFilter(audioName,btn , circle, boutoncolor, effet){
+
+    const effect = document.getElementById(effet);
+
+    context = (window.AudioContext) ? new AudioContext() : new window["webkitAudioContext"]();
+    let audioSource;
+    if (MEDIA_ELEMENT_NODES.has(document.getElementById(audioName))) {
+        audioSource = MEDIA_ELEMENT_NODES.get(document.getElementById(audioName))
+    } else {
+        audioSource = context.createMediaElementSource(document.getElementById(audioName))
+        MEDIA_ELEMENT_NODES.set(document.getElementById(audioName), audioSource)
+    }
+
+    let filter;
+
+    console.log(allAudioEffect)
+    if (allAudioEffect === undefined){
         filter = context.createBiquadFilter();
-    audioSource.connect(filter);
-    filter.connect(context.destination);
-    //filter.type = "lowshelf"
-    //filter.gain.value = 20
-    document.getElementById(circle).style.background = "red"
-    document.getElementById(boutoncolor).style.background = "red"
+        allAudioEffect = [[effet, filter]];
+    }
+    else {
+        filter = allAudioEffect[0][1];
+    }
+
+    try {
+        audioSource.connect(filter);
+        filter.connect(context.destination);
+        filter.type='bandpass';
+        filter.detune.value=parseInt(effect.value);
+    } catch (error) {
+        console.log("Nous sommes sur la bonne voie");
+        filter.detune.value = parseInt(effect.value)
+    }
+
 
 }
 
