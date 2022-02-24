42e0696b90a0fc5f990d665687ea953b4b5d76a8
julien-lab <julienn.ndiaye59155@gmail.com>
2022-02-24 20:51:42 +0100

    fix audio piste

--- public/js/connectedTableCoreFunction.js
+++ public/js/connectedTableCoreFunction.js
@@ -363,6 +363,8 @@ let exit = false;
 let canSuppress = true
 
 async function playComposition() {
+    // sert à arrêter les audioz avant le démarrage de la piste par exemple quand un audio est en train d'être écouté
+    stopComposition();
     if (!canPlay || soundsOnTracks.length === 0) return;
     socket.emit('changeSmartphoneDisplay', 'changeState');
     canPlay = false;
